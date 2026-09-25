34f317c10c445203d243504258de717088874a3f
jcs <jcs@jecc.ac.in>
2026-09-25 10:11:46 +0530

    fix: import useMemo in ComboFormModal

--- src/components/admin/ComboFormModal.jsx
+++ src/components/admin/ComboFormModal.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import { Modal } from '../ui/Modal';
 import { api } from '../../services/api';
 import { useToast } from '../../context/ToastContext';
